bc7c2d6d92888ef3f5a44d56d269ae4fd3be578b
Marina <marinasoon@gmail.com>
2024-03-29 19:50:25 +0300

    Passed test

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -17,7 +17,7 @@ public class PostmanEchoTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", org.hamcrest.Matchers.equalTo("some dat"));
+                .body("data", org.hamcrest.Matchers.equalTo("some data"));
         ;
     }
 
