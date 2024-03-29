77e0296662881e9119e3035d9be9875d9346d4f4
Marina <marinasoon@gmail.com>
2024-03-29 19:49:21 +0300

    Failed test

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Java CI with Gradle](https://github.com/MarinaaBogdanova/DZ2_3_postman_echo/actions/workflows/gradle.yml/badge.svg)](https://github.com/MarinaaBogdanova/DZ2_3_postman_echo/actions/workflows/gradle.yml)
---
+++ src/test/java/PostmanEchoTest.java
@@ -0,0 +1,24 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+
+public class PostmanEchoTest {
+
+    @Test
+    void test() {
+        String text = "Hi";
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", org.hamcrest.Matchers.equalTo("some dat"));
+        ;
+    }
+
+}
